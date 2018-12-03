c5096a17b85be1c59c00643bbf28041f833f7231
nraptis <nikos.raptis@thebeat.co>
2018-12-03 11:55:45 +0200

    Brackets

--- src/main/java/co/thebeat/ExerciseJ/Main.java
+++ src/main/java/co/thebeat/ExerciseJ/Main.java
@@ -10,8 +10,7 @@ public class Main {
         System.out.print("Input number of rows (half of the diamond) : ");
         Scanner in = new Scanner(System.in);
         r = in.nextInt();
-        for(i=0;i<=r;i++)
-        {
+        for(i=0;i<=r;i++) {
             for(j=1;j<=r-i;j++) {
                 System.out.print(" ");
                 System.out.print(j+","+i);
@@ -23,8 +22,7 @@ public class Main {
             System.out.print("\n");
         }
 
-        for(i=r-1;i>=1;i--)
-        {
+        for(i=r-1;i>=1;i--) {
             for(j=1;j<=r-i;j++)
                 System.out.print(" ");
             for(j=1;j<=2*i-1;j++)
