eacb018324778ed964b8ed1fe9131d60f1a85486
lydiatsagari <l.tsagari@thebeat.co>
2019-02-17 13:15:40 +0200

    Exercise Q

---
+++ src/main/java/co/thebeat/ExerciseQ/AlbumTests/CreateAlbumResponseResult.java
@@ -0,0 +1,38 @@
+package co.thebeat.ExerciseQ.AlbumTests;
+
+public class CreateAlbumResponseResult {
+
+    private String id;
+
+    private String title;
+
+    private String user_id;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+}
+
